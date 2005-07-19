e6cb274df47c1fec1062ac64a81225eec6355038
(no author) <(no author)@37e2af9d-f042-4f92-87bc-381327484f9f>
2005-07-19 07:36:33 +0000

    release 0.1.3 - fixes problems with changing source tag - closing tabs with an upload in progress no longer raises an exception
    git-svn-id: file:///tmp/svn/danbooru@28 37e2af9d-f042-4f92-87bc-381327484f9f

---
+++ danbooruup/tags/release-0.1.3/package.bat
@@ -0,0 +1,10 @@
+@echo off
+call vim -o install.rdf chrome\content\danbooruup\contents.rdf
+del /f danbooruup.xpi > nul
+del /f chrome\danbooruup.jar > nul
+cd chrome
+zip -9r danbooruup.jar .
+cd ..
+rem components\*.xpt components\*.js
+zip -9r danbooruup.xpi chrome.manifest install.rdf chrome\danbooruup.jar defaults
+
