486d05cdcfeca4405df3e5fa31b547dea7e008e0
buffered <buffered@37e2af9d-f042-4f92-87bc-381327484f9f>
2007-01-01 23:12:46 +0000

    release 0.2.7
    git-svn-id: file:///tmp/svn/danbooru@66 37e2af9d-f042-4f92-87bc-381327484f9f

---
+++ danbooruup/tags/release-0.2.7/chrome/content/danbooruup/danbooruUpBox.js
@@ -0,0 +1,66 @@
+function init()
+{
+	var prefSvc = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+	var ml = document.getElementById('danbooru');
+	ml.selectedIndex = -1;
+	ml.removeAllItems();
+	gDanbooruManager.init(ml);
+
+	document.getElementById('tags').focus();
+
+	if (prefSvc.getBoolPref("extensions.danbooruUp.fileurlsource") || !(window.arguments[0].imageURI.scheme == 'file') )
+	{
+		document.getElementById('source').value = window.arguments[0].imageURI.spec;
+	}
+}
+
+function doOK()
+{
+	var ml = document.getElementById('danbooru');
+	var tags = document.getElementById('tags').value;
+	var rating = ['Explicit','Questionable','Safe'][document.getElementById('ratinggrp').selectedIndex];
+	gDanbooruManager.selectDanbooru(ml.selectedIndex);
+	gDanbooruManager.uninit();
+
+	if(tags.length) {
+		// compact tag input
+		var tagarr=tags.replace(/\s\s+/g, ' ').replace(/^\s+|\s+$/g,'').split(' ');
+		var flat=[];
+		var needupdate = false;
+		for(var a in tagarr) {
+			flat[tagarr[a]]=null;
+		}
+		try { 		
+			var taghist = Components.classes["@mozilla.org/danbooru/taghistory-service;1"]
+					.getService(Components.interfaces.nsIDanbooruTagHistoryService);
+			for(var a in flat) {
+				if (!taghist.incrementValueForName(a))
+					needupdate = true;
+			}
+		} catch(e) {
+			// silently fail
+		}
+		var prefService		= Components.classes["@mozilla.org/preferences-service;1"]
+					.getService(Components.interfaces.nsIPrefBranch);
+		needupdate = prefService.getBoolPref("extensions.danbooruUp.autocomplete.update.afterdialog") && needupdate;
+	}
+
+	window.arguments[0].start(
+		window.arguments[0].imageURI,
+		document.getElementById('source').value,
+		tags,
+		rating,
+		ml.label,
+		window.arguments[0].imageNode,
+		window.arguments[0].wind,
+		needupdate);
+	window.arguments[0].imageNode = null;
+	window.arguments[0].wind = null;
+	window.arguments=null;
+
+	return true;
+}
+function doCancel() {
+	return true;
+}
+
---
+++ danbooruup/tags/release-0.2.7/defaults/preferences/danbooruup.js
@@ -0,0 +1,13 @@
+/* Default prefs for DanbooruUp */
+pref("extensions.danbooruUp.postadduri", "http://danbooru.donmai.us/api/add_post");
+pref("extensions.danbooruUp.postadduri.selected", 0);
+pref("extensions.danbooruUp.fileurlsource", false);
+pref("extensions.danbooruUp.updateuri", "http://danbooru.donmai.us/api/find_tags");
+pref("extensions.danbooruUp.autocomplete.enabled", true);
+pref("extensions.danbooruUp.autocomplete.update.onstartup", true);
+pref("extensions.danbooruUp.autocomplete.update.faststartup", true);
+pref("extensions.danbooruUp.autocomplete.update.beforedialog", false);
+pref("extensions.danbooruUp.autocomplete.update.afterdialog", true);
+pref("extensions.danbooruUp.autocomplete.update.ontimer", false);
+pref("extensions.danbooruUp.autocomplete.update.interval", 120);
+pref("extensions.danbooruUp.autocomplete.update.lastupdate", 0);
