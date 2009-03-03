2ec06dd9218499032e55b08546627688d8e6987c
albert <albert@37e2af9d-f042-4f92-87bc-381327484f9f>
2009-03-03 19:05:57 +0000

    * Deleted
    git-svn-id: file:///tmp/svn/danbooru@1920 37e2af9d-f042-4f92-87bc-381327484f9f

--- danbooru/branches/restbooru/app/models/post_tag_history.rb
+++
@@ -1,9 +0,0 @@
-class PostTagHistory < ActiveRecord::Base
-	def author
-		if user_id
-			connection.select_value("SELECT name FROM users WHERE id = #{self.user_id}")
-		else
-			CONFIG["default_guest_name"]
-		end
-	end
-end
