c62b96c7045953a0c20119c4f51bb91c6a60af90
albert <albert@37e2af9d-f042-4f92-87bc-381327484f9f>
2009-04-14 23:34:15 +0000

    removing old branch
    git-svn-id: file:///tmp/svn/danbooru@2033 37e2af9d-f042-4f92-87bc-381327484f9f

--- danbooru/branches/safebooru/app/models/advertisement_hit.rb
+++
@@ -1,3 +0,0 @@
-class AdvertisementHit < ActiveRecord::Base
-  belongs_to :advertisement
-end
--- danbooru/branches/safebooru/config/initializers/004_exception_notifier.rb
+++
@@ -1,3 +0,0 @@
-ExceptionNotifier.exception_recipients = [CONFIG["admin_contact"]]
-ExceptionNotifier.sender_address = CONFIG["admin_contact"]
-ExceptionNotifier.email_prefix = "[" + CONFIG["app_name"] + "] "
